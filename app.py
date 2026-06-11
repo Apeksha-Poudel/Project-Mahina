5193d72ad78d094cb07ca220abd170939a47d540
Apeksha-Poudel <poudel.apeksha65@gmail.com>
2026-06-11 15:12:07 +0545

    auto-commit: Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def donate():
 def get_donations():
     all_donations = list(donations.find({}, {"_id": 0}))
     return jsonify(all_donations)
-
+  
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
